511bbe80554ddafee05dc7483cb06e273222f644
Rubin Peci <rubinpeci@gmail.com>
2018-12-20 00:31:14 -0500

    .

--- basic_server.c
+++ basic_server.c
@@ -1,18 +1,38 @@
 #include "pipe_networking.h"
 
+#define SIGINT 2
+void sighandler(int n)
+{
+	if (n == SIGINT)
+	{
+		remove("mario");
+		printf("[server] piped removed, exiting!\n");
+		exit(0);
+	}
+}
 
 int main() {
 
   int to_client;
   int from_client;
+  char data[BUFFER_SIZE];
 
-  from_client = server_handshake( &to_client );
   printf("[server] waiting for data...\n");
 
-  while(from_client)
+  while(1)
   {
-	char data[BUFFER_SIZE];
-	read(from_client, data, BUFFER_SIZE);
-	printf("[server] received %s from client :3\n", data);
+	  from_client = server_handshake( &to_client );
+
+	  while(read(from_client, data, BUFFER_SIZE))
+	  {
+		printf("[server] received %s from client :3\n", data);
+
+		for(int i = 0; i < strlen(data); i++)
+			data[i] += 5;
+
+		write(to_client, data, BUFFER_SIZE);
+		printf("[server] sending back %s\n", data);
+	  }
+	  printf("[server] client disconnected\n");
   }
 }
--- client.c
+++ client.c
@@ -5,17 +5,20 @@ int main() {
 
   int to_server;
   int from_server;
+  char data[BUFFER_SIZE];
 
   from_server = client_handshake( &to_server );
 
   while(1)
   {
-	char data[BUFFER_SIZE];
-	
 	printf("[client] input something: ");
 	strncpy(data, readline(""), BUFFER_SIZE);
+
 	write(to_server, data, BUFFER_SIZE);
 	printf("\n[client] sent %s to server\n", data);
+
+	read(from_server, data, BUFFER_SIZE);
+	printf("[client] got back %s\n", data);
   }
 
 }
